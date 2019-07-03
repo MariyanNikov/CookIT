f339463f2d80009a73b118f6ca3592e82971d746
MariyanNikov <mariyan.nikov@abv.bg>
2019-07-03 23:39:04 +0300

    Setup EmailSender,Add Courier and Roles in the RolesSeeder.

--- CookIt.Common/GlobalConstants.cs
+++ CookIt.Common/GlobalConstants.cs
@@ -3,5 +3,9 @@
     public static class GlobalConstants
     {
         public const string AdministratorRoleName = "Administrator";
+
+        public const string CourierRoleName = "Courier";
+
+        public const string UserRoleName = "User";
     }
 }
--- Services/CookIt.Services.Messaging/NullMessageSender.cs
+++ Services/CookIt.Services.Messaging/NullMessageSender.cs
@@ -12,6 +12,7 @@
         public Task SendEmailAsync(string email, string subject, string message)
         {
             // Plug in your email service here to send an email.
+            // this.emailSender.SendEmailAsync(email, subject, message).GetAwaiter().GetResult();
             return Task.CompletedTask;
         }
 
