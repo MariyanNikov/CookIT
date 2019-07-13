34cab5e8faa408696365fdef9dd3e1d73bbd4d30
MariyanNikov <mariyan.nikov@abv.bg>
2019-07-14 00:18:03 +0300

    Fix minor bugs with AdminSeeder.cs.

---
+++ Data/CookIt.Data.Models/Address.cs
@@ -0,0 +1,19 @@
+﻿namespace CookIt.Data.Models
+{
+    using CookIt.Data.Common.Models;
+
+    public class Address : BaseModel<int>
+    {
+        public string StreetAddress { get; set; }
+
+        public string Description { get; set; }
+
+        public string City { get; set; }
+
+        public int CityCode { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ Data/CookIt.Data/Seeding/AdminSeeder.cs
@@ -0,0 +1,59 @@
+﻿namespace CookIt.Data.Seeding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using CookIt.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class AdminSeeder : ISeeder
+    {
+        private const string RootName = "root";
+
+        private const string RootPassword = "root123";
+
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            await SeedRootAdminAsync(userManager, RootName, RootPassword);
+        }
+
+        private static async Task SeedRootAdminAsync(UserManager<ApplicationUser> userManager, string adminName, string password)
+        {
+
+            var user = userManager.FindByEmailAsync($"{adminName}@{adminName}.com");
+            if (user == null)
+            {
+                var root = new ApplicationUser
+                {
+                    Email = $"{adminName}@{adminName}.com",
+                    EmailConfirmed = true,
+                    FirstName = $"{adminName}",
+                    LastName = $"{adminName}",
+                    PhoneNumber = "0889123456",
+                    UserName = $"{adminName}@{adminName}.com",
+                };
+                root.Claims.Add(new IdentityUserClaim<string> { ClaimType = "FullName", ClaimValue = $"{adminName}" });
+
+                var result = await userManager.CreateAsync(root, password);
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+                }
+
+                var addToRoleResult = await userManager.AddToRoleAsync(root, "Administrator");
+
+                if (!addToRoleResult.Succeeded)
+                {
+                    throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+                }
+            }
+
+        }
+    }
+}
